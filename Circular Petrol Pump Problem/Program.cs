cfc2827aacf8d4fa16ff823705490f30d7683c2b
Jaydeep Shil <ED8A@eurofins.com>
2020-09-24 12:27:59 +0530

    Add project files.

---
+++ Circular Petrol Pump Problem/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Circular_Petrol_Pump_Problem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var gas = new int[] { 1, 2, 3, 4, 5 };
+            var cost = new int[] { 3, 4, 5, 1, 2 };
+            Console.WriteLine(CanCompleteCircuit(gas, cost));
+        }
+
+        static int CanCompleteCircuit(int[] gas, int[] cost)
+        {
+            int sum = 0, position = 0, total = 0;
+
+            for (int i = 0; i < gas.Length; ++i)
+            {
+                sum = sum + gas[i] - cost[i];
+                if (sum < 0)
+                {
+                    total = total + sum;
+                    sum = 0;
+                    position = i + 1;
+                }
+            }
+
+            total = total + sum;
+            return total < 0 ? -1 : position;
+        }
+    }
+}
